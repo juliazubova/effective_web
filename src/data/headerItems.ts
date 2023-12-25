26ac64f037e67e7414e8d1e4dc6568bb7c23204a
Юлия Зубова <uzubova2020@gmail.com>
2023-12-25 18:33:34 +0600

    add navigation

---
+++ src/data/headerItems.ts
@@ -0,0 +1,31 @@
+import HeaderItem from "../models/NavigationItem";
+
+const headerItems: HeaderItem[] = [
+    {
+        text: 'Home',
+        link: '#',
+        isSelected: true,
+    },
+    {
+        text: 'Find a doctor',
+        link: '#',
+        isSelected: false,
+    },
+    {
+        text: 'Apps',
+        link: '#',
+        isSelected: false,
+    },
+    {
+        text: 'Testimonials',
+        link: '#',
+        isSelected: false,
+    },
+    {
+        text: 'About us',
+        link: '#',
+        isSelected: false,
+    },
+];
+
+export default headerItems;
