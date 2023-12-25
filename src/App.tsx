56497f6c0bb86597a000ee472a083d6231db6b4f
Юлия Зубова <uzubova2020@gmail.com>
2023-12-26 00:02:00 +0600

    add new sections

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Section from "./components/Section";
 import cardItems from "./data/cardItems";
 import navigationItems from "./data/navigationItems";
 import image_1 from './assets/image_1.png'
+import image_2 from './assets/image_2.png'
+import image_3 from './assets/image_3.png'
 
 function App() {
   return (
@@ -14,13 +16,31 @@ function App() {
         title="Virtual healthcare for you"
         description="Our service provides progressive, and affordable healthcare, accessible on mobile and online for everyone"
         image={image_1}
-        button = {<Button text='Consult today' isPrimaryColor={true} padding="48px 0px 0px 0px"/>}
+        button={<Button text='Consult today' isPrimaryColor={true} padding="48px 0px 0px 0px"/>}
+        isReverse={false}
+        hasDivider={false}
       />
       <CardSection 
         title="Our services"
         description="We provide to you the best choiches for you. Adjust it to your health needs and make sure your undergo treatment with our highly qualified doctors you can consult with us which type of service is suitable for your health"
         button={<Button text='Learn more' isPrimaryColor={false} padding="72px 0px 100px 0px"/>}
         items={cardItems}
+      />
+      <Section 
+        title="Leading healthcare providers"
+        description="We provides progressive, and affordable healthcare, accessible on mobile and online for everyone. To us, it’s not just work. We take pride in the solutions we deliver"
+        image={image_2}
+        button = {<Button text='Learn more' isPrimaryColor={false} padding="70px 0px 100px 0px"/>}
+        isReverse={true}
+        hasDivider={true}
+      />
+      <Section 
+        title="Download our mobile apps"
+        description="Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely"
+        image={image_3}
+        button = {<Button text='Download ↓' isPrimaryColor={false} padding="40px 0px 0px 0px"/>}
+        isReverse={false}
+        hasDivider={true}
       />
     </>
   )
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,28 +1,30 @@
 import styles from './section.module.css';
 import { ReactElement } from "react";
 
-const Section = (args: {title: string, description: string, image: string, button: ReactElement}) => {
+const Section = (args: {title: string, description: string, image: string, button: ReactElement, isReverse: boolean, hasDivider: boolean}) => {
     const text_column = <>
-        <div className={styles.section_title}>
-            <div className={styles.tile_title}>
+        <div className={styles.text_column}>
+            <div className={styles.section_title}>
                 {args.title}
             </div>
-            <div className={styles.tile_descripption}>
+            {args.hasDivider ? <hr/> : ''}
+            <div className={styles.section_description}>
                 {args.description}
             </div>
             {args.button}
         </div>
     </>
     const image = <>
-        <div className={styles.block_image}>
+        <div className={styles.section_image}>
             <img alt='image' src={args.image}/>
         </div>
     </>
 
     return <>
         <section className={styles.section}>
-            {text_column} 
-            {image}
+            {args.isReverse 
+                ? <>{image} {text_column}</> 
+                : <>{text_column} {image}</>}
         </section>
     </>
 }
