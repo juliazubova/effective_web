0fb012e046a22d626e4e216d3ffe8682d0b3de95
Юлия Зубова <uzubova2020@gmail.com>
2023-12-25 23:27:55 +0600

    add different button styles

--- src/App.tsx
+++ src/App.tsx
@@ -14,12 +14,12 @@ function App() {
         title="Virtual healthcare for you"
         description="Our service provides progressive, and affordable healthcare, accessible on mobile and online for everyone"
         image={image_1}
-        button = {<Button text='Consult today'/>}
+        button = {<Button text='Consult today' isPrimaryColor={true} padding="48px 0px 0px 0px"/>}
       />
       <CardSection 
         title="Our services"
         description="We provide to you the best choiches for you. Adjust it to your health needs and make sure your undergo treatment with our highly qualified doctors you can consult with us which type of service is suitable for your health"
-        button={<Button text='Learn more'/>}
+        button={<Button text='Learn more' isPrimaryColor={false} padding="72px 0px 100px 0px"/>}
         items={cardItems}
       />
     </>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,9 @@
 import styles from './button.module.css'
 
-const Button = (args: {text: string}) => {
+const Button = (args: {text: string, isPrimaryColor: boolean, padding: string}) => {
     return <>
-        <div style={{padding: "48px 0px 0px 0px"}}>
-            <a href={''} className={styles.button}>
+        <div style={{padding:  args.padding}}>
+            <a href={''} className={`${styles.button} ${args.isPrimaryColor ? styles.primary : styles.secondary}`}>
                 {args.text}
             </a>
         </div>
